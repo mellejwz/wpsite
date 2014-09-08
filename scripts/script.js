d7e23f9152fdc1e398e455293b3527d7ad125359
Melle Zwerver <mellezwerver@outlook.com>
2014-09-08 18:41:22 +0200

    added some app templates in applist and added back button functionality (just goes back to startscreen for now)

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
 
 	$('.arrow').click(function(){
 		$('#tiles').animate({left: "-=360"}, 600, 'easeOutCirc');
+		$('#display').animate({scrollTop:"0"}, 500, 'easeOutCirc');
 		$('#apps').animate({left: "-=360"}, 600, 'easeOutCirc');
+	});
+	$('#backbutton').click(function(){
+		$('#tiles').animate({left: "+=360"}, 600, 'easeOutCirc');
+		$('#display').animate({scrollTop:"0"}, 500, 'easeOutCirc');
+		$('#apps').animate({left: "+=360"}, 600, 'easeOutCirc');
 	});
 });
