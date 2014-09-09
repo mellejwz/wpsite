64c30945cb7b6d40cb9740067490022e691b9bd6
Melle Zwerver <mellezwerver@outlook.com>
2014-09-09 09:26:34 +0200

    removed scroll to top when going to applist

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 
 	$('.arrow').click(function(){
-		$('#tiles').animate({left: "-=360"}, 600, 'easeOutCirc');
-		$('#display').animate({scrollTop:"0"}, 500, 'easeOutCirc');
-		$('#apps').animate({left: "-=360"}, 600, 'easeOutCirc');
+		$('#tiles').animate({left: "-=360"}, 500, 'easeOutCirc');
+		$('#apps').animate({left: "-=360"}, 500, 'easeOutCirc');
 	});
 	$('#backbutton').click(function(){
-		$('#tiles').animate({left: "+=360"}, 600, 'easeOutCirc');
-		$('#display').animate({scrollTop:"0"}, 500, 'easeOutCirc');
-		$('#apps').animate({left: "+=360"}, 600, 'easeOutCirc');
+		$('#tiles').animate({left: "+=360"}, 500, 'easeOutCirc');
+		$('#apps').animate({left: "+=360"}, 500, 'easeOutCirc');
 	});
 });
